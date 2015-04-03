e2e2cf8832cf60f1032f9ad27bf3d544bc645be1
luvemil <taran.marco@gmail.com>
2015-04-03 16:35:00 +0200

    Add skeleton for a better class separation, untested

---
+++ crawler/forum.rb
@@ -0,0 +1,10 @@
+require 'nokogiri'
+require 'net/http'
+
+module BaseCrawler
+    class Forum < BaseCrawler::Crawler
+        def self.get_data node
+            #use old get_forums, get_threads and stuff
+        end
+    end
+end
