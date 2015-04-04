e2d097c4845c29114f96fed134a54bbaef846df0
luvemil <taran.marco@gmail.com>
2015-04-04 18:30:32 +0200

    Dummy copy of forums

--- crawler/forum/threads.rb
+++ crawler/forum/threads.rb
@@ -54,6 +54,56 @@ module BaseCrawler
                 return nil
             end
 
+            # BEGIN -- next page for forums
+            def self._index(url)
+                # Returns the last number in the url in case it means something (like
+                # the numbering of the posts in the forum) or 0 if it doesn't, and in
+                # this last case I assume that means we are dealing with the first page
+                # in a subforum spanning multiple pages.
+                return 0 unless /showtopic=\d+.*=\d+$/ =~ url
+                return /showtopic=\d+.*=(\d+)$/.match(url)[1].to_i
+            end
+
+            def self.get_nextpage node, url
+                # Returns a link to the next page.
+                # url is the current url, ideally @cur.data[:url] from a
+                # BaseCrawl object.
+                links = _get_possible_next node, url
+                if /showtopic=\d+$/.match url
+                    start = 0
+                else
+                    start = _index(url)
+                end
+                links.keep_if {|link| _index(link) > start}
+                links.uniq!
+                links.sort! {|a,b| _index(a) <=> _index(b)}
+                return links[0]
+            end
+
+            def self._get_possible_next node, url
+                # Get the pages which are candidates for being next in the forum.
+                # url is the URL of the current page.
+                links = []
+                node.css("tr td a").each do |tag_a|
+                    if /^\d+$/.match(tag_a.content) and _is_same_forum(url,tag_a["href"])
+                        links += [tag_a["href"]]
+                    end
+                end
+                return links
+            end
+
+            def self._is_same_forum(url, target)
+                # Returns true if target is in the same forum as url
+                t = /showtopic=(\d+)/.match(target)
+                if not t
+                    return false
+                end
+                n = /showtopic=(\d+)/.match(url)[1].to_i
+                m = t[1].to_i
+                return n == m
+            end
+            # END -- next page for forums
+
         end
     end
 end
