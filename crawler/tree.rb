23fabb1ebdaf0cde1ae8a06fb23443c286b7cf0e
luvemil <taran.marco@gmail.com>
2015-04-11 15:05:06 +0200

    Add some useful methods

--- crawler/tree.rb
+++ crawler/tree.rb
@@ -1,6 +1,6 @@
 require 'rubytree'
 module BaseCrawler
-    class Tree < ::Tree::TreeNode
+    class TreeNode < ::Tree::TreeNode
         attr_accessor :data, :children, :parent
         def initialize data = nil
             @children = []
@@ -12,9 +12,17 @@ module BaseCrawler
         end
 
         def add_child data
-            new_child = Tree.new data
+            new_child = TreeNode.new data
             new_child.parent = self
             @children += [ new_child ]
+        end
+    end
+
+    class Tree
+        attr_accessor :root
+        attr_reader :index
+        def initialize root = nil
+            @root = root
         end
 
         def self._gen_name name, index
@@ -22,6 +30,14 @@ module BaseCrawler
             # and an index (which should be updated once a child is added).
             # Returns [ child_name, index + 1 ]
             index ||= 0 # if is not define put it to zero
-            return [ "#{name}-#{index}", index + 1 ]
+            return "#{name}-#{index}"
+        end
+
+        def gen_name
+            # Fix root_name
+            name = Tree._gen_name <root_name>, @index
+            @index += 1
+            return name
+        end
     end
 end
